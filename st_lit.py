e5935ad63dccea554c767799c9f89fc404217c3c
danhorsley <danielhorsley@me.com>
2026-01-26 15:27:26 +0000

    more streamlit basics

--- st_lit.py
+++ st_lit.py
@@ -8,16 +8,19 @@ st.write("MAG 7 - strategy backtester")
 all_stocks = ["TSLA"]
 with st.container(border=True):
     stocks = st.multiselect("Stocks", all_stocks, default=all_stocks)
-    rolling_average = st.toggle("Rolling average")
+    portfolio_return = st.toggle("portfolio_return")
 
 np.random.seed(42)
 df = wrangle_data(pd.read_csv(f"data/{all_stocks[0]}.csv"))
 df = add_signals(df)
 df = find_returns(df)
 
-if rolling_average:
-    df = df['cumulative_rtn']
+
 
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-tab1.line_chart(df['Close'], height=250)
+if portfolio_return:
+    tab1.line_chart(df['cumulative_rtn'], height=250)
+else:
+    tab1.line_chart(df['Close'], height=250)
+
 tab2.dataframe(df, height=250, use_container_width=True)
