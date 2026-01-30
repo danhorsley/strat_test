a4cc3a75c9dc5d2b77f6dbc677d23ba0caeb04bb
danhorsley <danielhorsley@me.com>
2026-01-30 10:37:37 +0000

    functional parquet runner

--- parquet_cache.py
+++ parquet_cache.py
@@ -56,17 +56,22 @@ date_range = st.date_input(
     format="YYYY-MM-DD"  # optional, looks clean
 )
 
+
+
 # Handle the output
 if len(date_range) == 2:
     start_date, end_date = date_range
     st.write(f"Selected: {start_date} → {end_date}")
+    # Convert to pd.Timestamp for filtering (pandas likes Timestamp better than date)
+    start_ts = pd.Timestamp(start_date)
+    end_ts   = pd.Timestamp(end_date)
 else:
     st.info("Select both start and end dates")
 
 
 df_filt = df_pre[
-    (df_pre['Date'] >= start_date) &
-    (df_pre['Date'] <= end_date)
+    (df_pre['Date'] >= start_ts) &
+    (df_pre['Date'] <= end_ts)
 ].copy()
 
 df_filt['strat_rtn'] = df_filt[f'strat_rtn_{suffix}']
@@ -93,12 +98,12 @@ df_plot = df_filt.merge(
 
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
 if portfolio_return:
-    tab1.line_chart(df_plot, height=250)
+    tab1.line_chart(df_plot.drop_duplicates(subset='Date').set_index('Date')['port_cumulative_rtn'], height=250)
 else:
     df_filt['pct_change_close'] = df_filt.groupby('Ticker')['Close'].pct_change()
     df_filt['avg_pct_change'] = df_filt.groupby('Date')['pct_change_close'].transform('mean')
     df_filt['base_cum_rtn'] = (1 + df_filt['avg_pct_change']).cumprod()
-    tab1.line_chart(df_filt['base_cum_rtn'], height=250)
+    tab1.line_chart(df_filt.drop_duplicates(subset='Date').set_index('Date')['base_cum_rtn'], height=250)
 
 tab2.dataframe(df_filt, height=250, use_container_width=True)
 
